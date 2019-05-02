4a3c224514c748d3bdf94236b35bff40f16e60b1
Prava Dhulipalla <prava@students.olin.edu>
2019-05-01 20:39:18 -0400

    homework 4

--- homework4.py
+++ homework4.py
@@ -339,13 +339,7 @@ class RelationSorted:
         i = midblockIndex
 
 
-        #if this is the first element
-        #print("Out of search, where to insert,", i)
-        #print("numElementsToShift", numElementsToShift)
-        #print("prev", prev)
-        #print("tupleIndex", tupleIndex)
         while(numShifted <= numElementsToShift):
-            #print("here")
             if tupleIndex >= blockSize:
                 i = i+1
                 if (i>=numBlocks):
@@ -365,7 +359,6 @@ class RelationSorted:
                 file.push(i,block)
                 numShifted = numShifted + 1
                 tupleIndex = tupleIndex + 1
-
 
 
     def read_tuple (self,pkey):
@@ -452,26 +445,180 @@ class RelationIndexed:
 
     def primary_key_of (self,t):
 
-        indices = [ self._columns.index(c) for c in self._primary_key ]
+        indices = [self._columns.index(c) for c in self._primary_key]
         return tuple([ t[i] for i in indices])
+
+    def myLast(self,block):
+        blockSize = block.size()
+
+        i = 0
+        while (block.get(i) is not None):
+            i = i + 1
+            if (i >= blockSize):
+                return i-1
+        return i-1
+
+    def insert(self, blockIndex, insPos, tup):
+
+        file = self._tuples[1:]
+        numBlocks = file.size()
+        insPos = blockSize*(numBlocks-1) + 1
+
+        block =  file.pull(numBlocks-1)
+        blockSize = block.size()
+        last =  self.myLast(block)
+
+        end = (blockSize*(numBlocks-1)) + last + 1
+        numElementsToShift = end - (insPos-1)
+        numShifted = 0
+        prev = tup
+        i = blockIndex
+        block =  file.pull(blockIndex)
+
+        while(numShifted <= numElementsToShift):
+            if tupleIndex >= blockSize:
+                i = i+1
+                if (i>=numBlocks):
+                    file.allocate_block()
+                block = file.pull(i)
+                tupleIndex = 0
+                temp = prev
+                prev = block.get(tupleIndex)
+                block.put(tupleIndex,temp)
+                file.push(i,block)
+                numShifted = numShifted + 1
+                tupleIndex = tupleIndex + 1
+            else:
+                temp = prev
+                prev = block.get(tupleIndex)
+                block.put(tupleIndex,temp)
+                file.push(i,block)
+                numShifted = numShifted + 1
+                tupleIndex = tupleIndex + 1
 
     def create_tuple (self,tup):
 
-        raise Exception("Not implemented")
+        file = self._tuples
+        firstBlock = file.pull(0)
+        blockSize = firstBlock.size()
+        tupleWithIndices = firstBlock.get(0)
+        pkey = self.primary_key_of(tup)
+
+
+        #If there is no tuple in the first block, create a tuple and insert it to the block
+        if tupleWithIndices is None:
+            tupleWithIndices = (pkey,)
+            firstBlock.put(tupleWithIndices) "needs another value"
+            file.push(firstBlock)
+            return
+
+        idx = 0 #tuple index
+        blockidx = 1 #block index
+        finalblockidx = 0
+
+        primaryKeyInIndex = tuplesIndices[idx]
+
+        while(primaryKeyInIndex > pkey):
+            idx = idx + 1
+            blockidx = idx + 1
+
+            if idx >= len(tuplesWithIndices):
+                #The tuple has to be inserted at the last
+                blockidx =  file.size()-1
+                block = file.pull(blockidx)
+                if block.pull(blockidx).has_space():
+                    #if self.myLast(block) < blockSize - 1:
+                        block.put(self.myLast(block)+1,tuple)
+                else:
+                    file.allocate_block()
+                    blockSize += 1
+                    block = file.pull(blockidx+1)
+                    file.push(block)
+
+            primaryKeyInIndex = tuplesIndices[idx]
+
+        if (primaryKeyInIndex == pkey):
+            raise Exception ("Tuple with primary key already exists")
+            return
+
+        finalblockidx = blockidx - 1
+        if (finalblockidx < 1):
+            finalblockidx = blockidx
+            self.insert(1,0,tup)
+        else:
+            prevBlock = file.pull(finalblockidx)
+            prevlasttup = prevBlock.get(prevBlock.size() - 1)
+            prevlastpkey = self.primary_key_of(prevlasttup)
+
+            if (prevlastpkey < pkey):
+                finalblockidx = blockidx
+
 
     def read_tuple (self,pkey):
+        file = self._tuples
+
+        indexBlock = file.pull(0) #supposed to be the first block
+        tuplesWithIndices = indexBlock.get(0)
+
+        for i in range(0, len(tuplesWithIndices)): #for each element of the tuple
+            if tuplesWithIndices[i] == pkey: #if the elemet we are on is == to pkey
+                block.get(i+1) #get the block at index i+1
 
         raise Exception("Not implemented")
 
     def delete_tuple (self,pkey):
+
+        file = self._tuples
+        firstBlock = file.pull(0)
+        tuplesWithIndices = firstBlock.get(0)
+
+        if tuplesWithIndices is None:
+            raise Exception("The file is empty")
+            return
+
+        if (len(tuplesWithIndices) == 1):
+            # delete tuple
+            return
+
+        blockidx = 0
+        finalblockidx = 0
+
+        for i in range(0, len(tuplesWithIndices)):
+            if (tuplesWithIndices[i] < pkey):
+                if i == 0:
+                    blockidx = i
+                else:
+                    blockidx = i - 1
+                break
+
+        deleteBlock = file.pull(blockidx)
+
+        deleteidx = 0
+        flag = 0
+        for i in range(0, deleteBlock.size()):
+            tup = deleteBlock.get(i)
+            thispkey = self.primary_key_of(tup)
+            if (thispkey == pkey):
+                deleteidx = i
+                if (deleteidx != deleteBlock.size() - 1):
+                    flag = 1
+            if flag and (i != deleteBlock.size() - 1):
+                deleteBlock.get(i + 1) #set deleteBlock[i] = deleteBlock[i+1]
+
+
+        # actually delete (done ? above)
+        # shift elements in each block to the left (done for the delete block above)
+        # shift block up one element
+        # update the index tuple
+
+        for i in range(blockidx + 1, len(file)):
+            for k in range(0, )
 
         raise Exception("Not implemented")
 
     def statistics (self):
 
         return self._tuples.statistics()
-
-
 
 ############################################################
 # API to files and blocks
@@ -709,10 +856,10 @@ print(books.read_tuple(("0060558121",)))
 print(books.read_tuple(("0307274939",)))
 books.delete_tuple(("0060558121",))
 '''
-books = RelationSorted(["title","year","numberPages","isbn"],["isbn"])
+#books = RelationSorted(["title","year","numberPages","isbn"],["isbn"])
 
 
-books.create_tuple(( "The American Civil War", 2009, 396, "0307274939"))
+'''books.create_tuple(( "The American Civil War", 2009, 396, "0307274939"))
 books.create_tuple(( "A Distant Mirror", 1972, 677, "0345349571"))
 books.create_tuple(( "The Guns of August", 1962, 511, "034538623X"))
 books.create_tuple(( "Norse Mythology", 2017, 299, "0393356182"))
@@ -728,9 +875,8 @@ books.delete_tuple(("0393356182",))
 books.delete_tuple(("0060853980",))
 books.delete_tuple(("0307274939",))
 #books.read_tuple(("0307books.delete_tuple(("0393356182",))
+'''
 
-
-print(books._tuples)
 
 
 
@@ -757,23 +903,29 @@ print(books._tuples)
 #                              ( "The Poisonwood Bible", 1998, 560, "0060175400")
 #                          ])
 
-# BOOKS = RelationIndexed(["title","year","numberPages","isbn"],
-#                         ["isbn"],
-#                         [
-#                             ( "A Distant Mirror", 1972, 677, "0345349571"),
-#                             ( "The Guns of August", 1962, 511, "034538623X"),
-#                             ( "Norse Mythology", 2017, 299, "0393356182"),
-#                             ( "American Gods", 2003, 591, "0060558121"),
-#                             ( "The Ocean at the End of the Lane", 2013, 181, "0062255655"),
-#                             ( "Good Omens", 1990, 432, "0060853980"),
-#                             ( "The American Civil War", 2009, 396, "0307274939"),
-#                             ( "The First World War", 1999, 500, "0712666451"),
-#                             ( "The Kidnapping of Edgardo Mortara", 1997, 350, "0679768173"),
-#                             ( "The Fortress of Solitude", 2003, 509, "0375724886"),
-#                             ( "The Wall of the Sky, The Wall of the Eye", 1996, 232, "0571205992"),
-#                             ( "Stories of Your Life and Others", 2002, 281, "1101972120"),
-#                             ( "The War That Ended Peace", 2014, 739, "0812980660"),
-#                             ( "Sheaves in Geometry and Logic", 1994, 630, "0387977102"),
-#                             ( "Categories for the Working Mathematician", 1978, 317, "0387984032"),
-#                             ( "The Poisonwood Bible", 1998, 560, "0060175400")
-#                         ])
+BOOKS = RelationIndexed(["title","year","numberPages","isbn"],
+                     ["isbn"],
+                     [
+                         ( "A Distant Mirror", 1972, 677, "0345349571"),
+                         ( "The Guns of August", 1962, 511, "034538623X"),
+                         ( "Norse Mythology", 2017, 299, "0393356182"),
+                         ( "American Gods", 2003, 591, "0060558121"),
+                         ( "The Ocean at the End of the Lane", 2013, 181, "0062255655"),
+                         ( "Good Omens", 1990, 432, "0060853980"),
+                         ( "The American Civil War", 2009, 396, "0307274939"),
+                         ( "The First World War", 1999, 500, "0712666451"),
+                         ( "The Kidnapping of Edgardo Mortara", 1997, 350, "0679768173"),
+                         ( "The Fortress of Solitude", 2003, 509, "0375724886"),
+                         ( "The Wall of the Sky, The Wall of the Eye", 1996, 232, "0571205992"),
+                         ( "Stories of Your Life and Others", 2002, 281, "1101972120"),
+                         ( "The War That Ended Peace", 2014, 739, "0812980660"),
+                         ( "Sheaves in Geometry and Logic", 1994, 630, "0387977102"),
+                         ( "Categories for the Working Mathematician", 1978, 317, "0387984032"),
+                         ( "The Poisonwood Bible", 1998, 560, "0060175400")])
+
+#books.insert(0,0,( "A Distant Mirror", 1972, 677, "0345349571"))
+#books.create_tuple(( "The American Civil War", 2009, 396, "0307274939"))
+#books.create_tuple(( "A Distant Mirror", 1972, 677, "0345349571"))
+#books.create_tuple(( "The Guns of August", 1962, 511, "034538623X"))
+#books.create_tuple(( "Norse Mythology", 2017, 299, "0393356182"))
+#print(books._tuples)
